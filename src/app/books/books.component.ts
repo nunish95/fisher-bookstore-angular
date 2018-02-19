7be6281ffd1dbeb09d1b3203e9e4c701a5f4bc0a
Anqi <yang.2740@osu.edu>
2018-02-19 15:50:11 -0500

    Added components and mock services

---
+++ src/app/books/books.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import {Book} from '../books/book';
+import {BOOKS} from '../books/mock-book-service'
+@Component({
+  selector: 'app-books',
+  templateUrl: './books.component.html',
+  styleUrls: ['./books.component.css']
+})
+export class BooksComponent implements OnInit {
+ 
+  book=BOOKS;
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
